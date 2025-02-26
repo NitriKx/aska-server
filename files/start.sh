1e6e037899ba4289019d4fe62fe0f6967c157abc
luxusburg <git@luxusburg.lu>
2025-02-26 18:52:16 +0100

    Adding winetricks

--- files/start.sh
+++ files/start.sh
@@ -66,4 +66,4 @@ echo "Starting Aska Dedicated Server"
 echo " "
 echo "Launching wine Aska"
 echo " "
-xvfb-run -e /dev/stdout wine $server_files/AskaServer.bat 2>&1
+xvfb-run wine $server_files/AskaServer.exe -propertiesPath 'server properties.txt' 2>&1
